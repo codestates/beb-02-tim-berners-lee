ecf55e30f0e4be2a2c7b88f56f93e93b4e0d6d46
choihocheol <show1693@gmail.com>
2022-02-16 17:35:19 +0900

    NFT 기능 구현
    보유 NFT display, NFT 전송 기능 구현

--- project/src/App.js
+++ project/src/App.js
@@ -6,12 +6,14 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MyNFT from './pages/MyNFT';
 import { initialState } from './assets/state';
+import { fidenzaAbi, fidenzaAddr } from './fidenzaContract';
+import { sealenzaAbi, sealenzaAddr } from './sealenzaContract';
 
 function App() {
   const [web3, setWeb3] = useState();
   const [account, setAccount] = useState('');
   const [nfts, setNFTs] = useState(initialState.arts);
-  const [myNFTs, setMyNFTs] = useState(initialState.cartItems);
+  const [myNFTs, setMyNFTs] = useState([]);
   const [token, setToken] = useState({
     name: undefined,
     symbol: undefined,
@@ -29,6 +31,35 @@ function App() {
     }
   }, []);
 
+  const renewMyNFTList = async () => {
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+    const contracts = [
+      [fidenzaAbi, fidenzaAddr],
+      [sealenzaAbi, sealenzaAddr],
+    ];
+    for (const [abi, address] of contracts) {
+      const contract = new web3.eth.Contract(abi, address);
+      const name = await contract.methods.name().call();
+      const totalSupply = await contract.methods.totalSupply().call();
+
+      setMyNFTs([]);
+      for (let tokenId = 1; tokenId <= totalSupply; tokenId++) {
+        const tokenOwner = await contract.methods.ownerOf(tokenId).call();
+        if (tokenOwner === accounts[0]) {
+          const tokenPrice = await contract.methods.tokenPrice(tokenId).call();
+          const tokenURI = await contract.methods.tokenURI(tokenId).call();
+          setMyNFTs((prevState) => {
+            return [
+              ...prevState,
+              { contract, name, tokenPrice, tokenId, tokenURI },
+            ];
+          });
+        }
+      }
+    }
+  };
+
   return (
     <Router>
       <Nav
@@ -38,6 +69,7 @@ function App() {
         setAccount={setAccount}
         token={token}
         setToken={setToken}
+        renewMyNFTList={renewMyNFTList}
       />
       <Switch>
         <Route exact={true} path="/">
@@ -49,7 +81,12 @@ function App() {
           />
         </Route>
         <Route path="/myNFT">
-          <MyNFT myNFTs={myNFTs} setMyNFTs={setMyNFTs} nfts={nfts} />
+          <MyNFT
+            myNFTs={myNFTs}
+            setMyNFTs={setMyNFTs}
+            nfts={nfts}
+            account={account}
+          />
         </Route>
       </Switch>
     </Router>
--- project/src/components/MyItem.js
+++ project/src/components/MyItem.js
@@ -1,29 +1,54 @@
-import React from 'react'
+import { useState } from 'react';
 
 export default function MyItem({
   item,
   handleAddressChange,
   handleTransfer,
-  quantity
+  quantity,
+  account,
 }) {
+  const [recipient, setRecipient] = useState('');
+
+  const sendToken = (tokenId) => {
+    item.contract.methods
+      .transferFrom(account, recipient, tokenId)
+      .send({
+        from: account,
+      })
+      .then(() => {
+        setRecipient('');
+      });
+  };
 
   return (
     <li className="cart-item-body">
       <div className="cart-item-thumbnail">
-        <img src={item.img} alt={item.name} />
+        <img src={item.tokenURI} alt={item.name} />
       </div>
       <div className="cart-item-info">
-        <div className="cart-item-title" data-testid={item.name}>{item.name}</div>
-        <div className="cart-item-price">⧫ {item.price}</div>
+        <div className="cart-item-title" data-testid={item.name}>
+          {item.name} #{item.tokenId}
+        </div>
+        <div className="cart-item-price">⧫ {item.tokenPrice}</div>
       </div>
       <input
         className="cart-item-quantity"
-        value={quantity}
+        value={recipient}
+        // value={quantity}
         onChange={(e) => {
-          handleAddressChange(Number(e.target.value), item.id)
-        }}>
-      </input>
-      <button className="cart-item-delete" onClick={() => { handleTransfer(item.itemId) }}>전송</button>
-    </li >
-  )
+          setRecipient(e.target.value);
+          // handleAddressChange(Number(e.target.value), item.id);
+        }}
+      ></input>
+      <button
+        className="cart-item-delete"
+        onClick={() => {
+          sendToken(item.tokenId);
+          // handleTransfer(item.itemId);
+        }}
+      >
+        전송
+      </button>
+    </li>
+  );
 }
